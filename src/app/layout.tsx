007d61bb38aba350fde8314f03e4f99b6c67295c
Nate van der Vis <natevandervis@gmail.com>
2024-05-25 10:29:27 -0500

    🏗 building: adding favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,6 +18,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <link rel="icon" href="/favicon.png" />
       <body className={inter.className}>{children}</body>
     </html>
   );
