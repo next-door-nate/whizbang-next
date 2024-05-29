31a9b12140d136a421692622184ec406ce0b2329
next-door-nate <natevandervis@gmail.com>
2024-05-29 11:11:06 -0500

    🏗️ building: adding dropdown nav support

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 
 import Link from "next/link";
 import Container from "./Container";
@@ -18,9 +18,8 @@ type HeaderProps = {
 export default function Header({ header }: HeaderProps) {
   const [showMenu, setShowMenu] = useState(false);
 
-
-  function toggleMenu(){
-    console.log('woooo')
+  function toggleMenu() {
+    console.log("woooo");
   }
 
   return (
@@ -38,38 +37,86 @@ export default function Header({ header }: HeaderProps) {
                 <nav>
                   {header.nav.map((item: any, i: number) => {
                     return (
-                      <div key={item._key} className={styles.item} onMouseEnter={item.link.linklist?.length > 0 ? () => {setShowMenu(!showMenu)}: null} onMouseLeave={item.link.linklist?.length > 0 ? () => {setShowMenu(!showMenu)}: null} onClick={item.link.linklist?.length > 0 ? () => {setShowMenu(!showMenu)}: null}>
+                      <div
+                        key={item._key}
+                        className={styles.item}
+                        onMouseEnter={
+                          item.link.linklist?.length > 0
+                            ? () => {
+                                setShowMenu(!showMenu);
+                              }
+                            : undefined
+                        }
+                        onMouseLeave={
+                          item.link.linklist?.length > 0
+                            ? () => {
+                                setShowMenu(!showMenu);
+                              }
+                            : undefined
+                        }
+                        onClick={
+                          item.link.linklist?.length > 0
+                            ? () => {
+                                setShowMenu(!showMenu);
+                              }
+                            : undefined
+                        }
+                      >
                         {item.link.linklist?.length > 0 ? (
                           <>
-                          <button title={item.link.title}>
-                            {item.link.title}
-                            <svg width="8" height="5" viewBox="0 0 8 5" fill="none" xmlns="http://www.w3.org/2000/svg">
-                              <path d="M1 1L4 4L7 1" stroke="black"/>
-                            </svg>
-                          </button>
-                          <nav className={styles.dropdown} data-show={showMenu}>
-                            {item.link.linklist.map((sublink: any) => {
-                              return (
-                                <div key={sublink._key}>
-                                  <Link href={sublink.external_link ? sublink.external_link : linkResolver(sublink.link)} title={sublink.title}>{sublink.title}</Link>
+                            <button title={item.link.title}>
+                              {item.link.title}
+                              <svg
+                                width="8"
+                                height="5"
+                                viewBox="0 0 8 5"
+                                fill="none"
+                                xmlns="http://www.w3.org/2000/svg"
+                              >
+                                <path d="M1 1L4 4L7 1" stroke="black" />
+                              </svg>
+                            </button>
+                            <nav
+                              className={styles.dropdown}
+                              data-show={showMenu}
+                              onBlur={
+                                item.link.linklist?.length > 0
+                                  ? () => {
+                                      setShowMenu(!showMenu);
+                                    }
+                                  : undefined
+                              }
+                            >
+                              {item.link.linklist.map((sublink: any) => {
+                                return (
+                                  <div key={sublink._key}>
+                                    <Link
+                                      href={
+                                        sublink.external_link
+                                          ? sublink.external_link
+                                          : linkResolver(sublink.link)
+                                      }
+                                      title={sublink.title}
+                                    >
+                                      {sublink.title}
+                                    </Link>
                                   </div>
-                              )
-                            })}
-                          </nav>
+                                );
+                              })}
+                            </nav>
                           </>
-                        )
-                        :
-                        <Link
-                          href={
-                            item.link.external_link
-                              ? item.link.external_link
-                              : linkResolver(item.link)
-                          }
-                          title={item.link.title}
-                        >
-                          {item.link.title}
-                        </Link>
-                      }
+                        ) : (
+                          <Link
+                            href={
+                              item.link.external_link
+                                ? item.link.external_link
+                                : linkResolver(item.link)
+                            }
+                            title={item.link.title}
+                          >
+                            {item.link.title}
+                          </Link>
+                        )}
                       </div>
                     );
                   })}
@@ -77,15 +124,27 @@ export default function Header({ header }: HeaderProps) {
               )}
             </div>
 
-            {header.ctas.length > 0 && 
-            <nav className={styles.right}>
-              {header.ctas.map((item, i) => {
-                return(
-                  <Link key={item._key} href={item.external_link ? item.external_link : linkResolver(item.link)} className="button" data-button={i == 0 ? "primary" : "text"} title={item.text}>{item.text}</Link>
-                )
-              })}
-            </nav>
-            }
+            {header.ctas.length > 0 && (
+              <nav className={styles.right}>
+                {header.ctas.map((item, i) => {
+                  return (
+                    <Link
+                      key={item._key}
+                      href={
+                        item.external_link
+                          ? item.external_link
+                          : linkResolver(item.link)
+                      }
+                      className="button"
+                      data-button={i == 0 ? "primary" : "text"}
+                      title={item.text}
+                    >
+                      {item.text}
+                    </Link>
+                  );
+                })}
+              </nav>
+            )}
           </div>
         </Container>
       </header>
